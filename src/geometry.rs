421d987ffdd0ef139d9087a3c9f270a3268c5adf
Niall FitzGibbon <fitzgibbon.niall@gmail.com>
2019-05-15 17:16:12 +0100

    Length comparisons (Vector3.norm not defined for isize scalars).

--- src/geometry.rs
+++ src/geometry.rs
@@ -1,14 +1,26 @@
 use nalgebra::Vector3;
+use std::cmp::Ordering;
 
 /// We use cube coordinates as described at https://www.redblobgames.com/grids/hexagons/ and https://www.redblobgames.com/grids/hexagons/implementation.html
-trait HexCoord {
-    fn new(q: isize, r: isize, s: isize) -> Self;
+trait Coord {
+    type Scalar;
+    fn cmp_len(&self, length: Self::Scalar) -> Ordering;
 }
 
-impl HexCoord for Vector3<isize> {
-    fn new(q: isize, r: isize, s: isize) -> Self {
-        assert_eq!(q + r + s, 0);
-        Vector3::new(q, r, s)
+type Hex = Vector3<isize>;
+
+impl Coord for Hex {
+    type Scalar = isize;
+    fn cmp_len(&self, length: Self::Scalar) -> Ordering {
+        let asq = (self.x * self.x) + (self.y * self.y) + (self.z * self.z);
+        let bsq = length * length;
+        if asq < bsq {
+            Ordering::Less
+        } else if asq == bsq {
+            Ordering::Equal
+        } else {
+            Ordering::Greater
+        }
     }
 }
 
@@ -24,6 +36,16 @@ mod test {
     use super::*;
     #[test]
     fn make_valid_hex_coordinate() {
-        let _pos: Vector3<isize> = HexCoord::new(2, 3, -5);
+        let a = Hex::new(2, 3, -5);
+        let b = Hex::new(-3, 1, 2);
+        println!("a -> b: {}", b - a);
+        println!(
+            "length compare to 8: {:?}, to 9: {:?}",
+            (b - a).cmp_len(8),
+            (b - a).cmp_len(9)
+        );
+        assert_eq!(b - a, Hex::new(-5, -2, 7));
+        assert_eq!((b - a).cmp_len(8), Ordering::Greater);
+        assert_eq!((b - a).cmp_len(9), Ordering::Less);
     }
 }
