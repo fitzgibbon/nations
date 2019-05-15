862242054c5c2473a227e1ba95cf129f48fd7ada
Niall FitzGibbon <fitzgibbon.niall@gmail.com>
2019-05-15 18:29:19 +0100

    Manhattan walk iterator.

--- src/geometry.rs
+++ src/geometry.rs
@@ -1,10 +1,12 @@
 use nalgebra::Vector3;
-use std::cmp::Ordering;
+use std::cmp::{max, min, Ordering};
 
 /// We use cube coordinates as described at https://www.redblobgames.com/grids/hexagons/ and https://www.redblobgames.com/grids/hexagons/implementation.html
 trait Coord {
     type Scalar;
     fn cmp_len(&self, length: Self::Scalar) -> Ordering;
+    fn manhattan(&self) -> Self::Scalar;
+    fn manhattan_iter(length: Self::Scalar) -> Box<Iterator<Item = Self>>;
 }
 
 type Hex = Vector3<isize>;
@@ -21,6 +23,48 @@ impl Coord for Hex {
         } else {
             Ordering::Greater
         }
+    }
+
+    fn manhattan(&self) -> Self::Scalar {
+        (self.x.abs() + self.y.abs() + self.z.abs()) / 2
+    }
+
+    fn manhattan_iter(length: Self::Scalar) -> Box<Iterator<Item = Self>> {
+        Box::new(HexManhattanIterator::new(length))
+    }
+}
+
+struct HexManhattanIterator {
+    x: isize,
+    y: isize,
+    length: isize,
+}
+
+impl HexManhattanIterator {
+    fn new(length: isize) -> Self {
+        HexManhattanIterator {
+            x: -length,
+            y: 0,
+            length,
+        }
+    }
+}
+
+impl Iterator for HexManhattanIterator {
+    type Item = Hex;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let mut result = None;
+
+        if self.y > min(self.length, self.length - self.x) {
+            self.x += 1;
+            self.y = max(-self.length, -self.x - self.length)
+        }
+        if self.x <= self.length {
+            result = Some(Hex::new(self.x, self.y, -self.x - self.y));
+        }
+        self.y += 1;
+        result
     }
 }
 
@@ -47,5 +91,18 @@ mod test {
         assert_eq!(b - a, Hex::new(-5, -2, 7));
         assert_eq!((b - a).cmp_len(8), Ordering::Greater);
         assert_eq!((b - a).cmp_len(9), Ordering::Less);
+        println!("manhattan a -> b: {}", (b - a).manhattan());
+        assert_eq!((b - a).manhattan(), 7);
+    }
+
+    #[test]
+    fn walk_manhattan() {
+        println!(
+            "{}",
+            HexManhattanIterator::new(2)
+                .map(|x| format!("{}", x))
+                .collect::<Vec<_>>()
+                .join(" ")
+        );
     }
 }
